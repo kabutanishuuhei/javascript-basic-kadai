caee32c41c1ed090d223708cae7f5bd6e91a498a
しゅうまい <kabutani_shuuhei@koushi-intec.co.jp>
2025-06-03 17:22:01 +0900

    Update mdn.js

--- kadai_mdn_js/mdn.js
+++ kadai_mdn_js/mdn.js
@@ -2,8 +2,6 @@ const now = new Date();
   
 const year = now.getFullYear();      // 年を取得するインスタンスメソッド  
 const month = now.getMonth() + 1;    // 月は0〜11なので＋1  
-const day = now.getDate();            // 日  
-const hour = now.getHours();          // 時  
-const minute = now.getMinutes();     // 分  
+const day = now.getDate();            // 日   
   
-console.log(`${year}年${month}月${day}日 ${hour}時${minute}分`);  
+console.log(`${year}年${month}月${day}日`);  
